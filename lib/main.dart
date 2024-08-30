24de8f4a580db12b2536e348f6c364ad1f20bdc1
jeongmi97 <kjm7943@naver.com>
2024-08-30 17:30:26 +0900

    title 화면전환

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:jmc/screens/home_screen.dart';
 import 'package:jmc/screens/title_screen.dart';
 
 void main() {
@@ -17,17 +18,23 @@ class MyApp extends StatelessWidget {
         textTheme: const TextTheme(
           displayLarge: TextStyle(fontWeight: FontWeight.bold),
           displaySmall: TextStyle(
-              fontWeight: FontWeight.bold,
-              color: Colors.white,
-              shadows: [
-                Shadow(
-                  blurRadius: 1,
-                  color: Color.fromRGBO(0, 0, 0, 0.2),
-                  offset: Offset(0, 4),
-                ),
-              ]),
+            fontWeight: FontWeight.bold,
+            color: Colors.white,
+            shadows: [
+              Shadow(
+                blurRadius: 1,
+                color: Color.fromRGBO(0, 0, 0, 0.2),
+                offset: Offset(0, 4),
+              ),
+            ],
+          ),
         ),
       ),
+      initialRoute: '/title',
+      routes: {
+        '/title': (context) => const TitleScreen(),
+        '/home': (context) => const HomeScreen(),
+      },
       home: const TitleScreen(),
     );
   }
--- lib/screens/title_screen.dart
+++ lib/screens/title_screen.dart
@@ -5,35 +5,50 @@ class TitleScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Theme.of(context).canvasColor,
-      body: SafeArea(
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Text(
-                    "점\n메\n추",
-                    style: Theme.of(context).textTheme.displaySmall,
-                  ),
-                  const Icon(
-                    Icons.horizontal_rule,
-                    size: 100,
-                    color: Colors.white,
-                  ),
-                  Text(
-                    "심\n뉴\n천",
-                    style: Theme.of(context).textTheme.displaySmall,
-                  )
-                ],
-              ),
-            ],
-          ),
+    return FutureBuilder(
+        future: Future.delayed(
+          const Duration(seconds: 2),
+          () => Navigator.pushNamed(context, '/home'),
         ),
-      ),
-    );
+        builder: (context, snapshot) {
+          print('called builder');
+          return Scaffold(
+            backgroundColor: Theme.of(context).canvasColor,
+            body: SafeArea(
+              child: Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          "점\n메\n추",
+                          style: Theme.of(context).textTheme.displaySmall,
+                        ),
+                        const Icon(
+                          Icons.horizontal_rule,
+                          size: 100,
+                          color: Colors.white,
+                          // shadows: [
+                          //   Shadow(
+                          //     blurRadius: 1,
+                          //     color: Color.fromRGBO(0, 0, 0, 0.2),
+                          //     offset: Offset(0, 4),
+                          //   ),
+                          // ],
+                        ),
+                        Text(
+                          "심\n뉴\n천",
+                          style: Theme.of(context).textTheme.displaySmall,
+                        )
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          );
+        });
   }
 }
